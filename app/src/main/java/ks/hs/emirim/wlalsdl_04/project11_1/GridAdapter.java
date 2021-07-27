3cb100e0766c96d16035b0769ef3b24bce927d1d
jimin <w2037@e-mirim.hs.kr>
2021-07-27 11:40:09 +0900

    Completion

--- app/src/main/java/ks/hs/emirim/wlalsdl_04/project11_1/GridAdapter.java
+++ app/src/main/java/ks/hs/emirim/wlalsdl_04/project11_1/GridAdapter.java
@@ -10,6 +10,7 @@ import android.widget.ImageView;
 public class GridAdapter extends BaseAdapter {
     Context context;
     int[] posterIds = {R.drawable.i1,R.drawable.i2,R.drawable.i3,R.drawable.i4,R.drawable.i5,R.drawable.i6,R.drawable.i7,R.drawable.i8,R.drawable.i9,R.drawable.i10,R.drawable.i1,R.drawable.i2,R.drawable.i3,R.drawable.i4,R.drawable.i5,R.drawable.i6,R.drawable.i7,R.drawable.i8,R.drawable.i9,R.drawable.i10};
+    int[] titleIds = {R.string.poster_title1,R.string.poster_title2,R.string.poster_title3,R.string.poster_title4,R.string.poster_title5,R.string.poster_title6,R.string.poster_title7,R.string.poster_title8,R.string.poster_title9,R.string.poster_title10,R.string.poster_title1,R.string.poster_title2,R.string.poster_title3,R.string.poster_title4,R.string.poster_title5,R.string.poster_title6,R.string.poster_title7,R.string.poster_title8,R.string.poster_title9,R.string.poster_title10};
 
     public  GridAdapter(Context context){
         this.context = context;
@@ -47,6 +48,7 @@ public class GridAdapter extends BaseAdapter {
                 ImageView imgvLarge = dialogView.findViewById(R.id.imgv_Large);
                 imgvLarge.setImageResource(posterIds[pos]);
                 dialog.setTitle("Large Poster");
+                dialog.setTitle(titleIds[pos]);
                 dialog.setIcon(R.drawable.fi);
                 dialog.setView(dialogView);
                 dialog.setNegativeButton("close",null);
